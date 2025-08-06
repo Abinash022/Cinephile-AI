039db71619f829fb81d938c3e1c9f5537b0eba85
Abinash022 <abinashkarki16@gmail.com>
2025-08-06 17:27:19 +0545

    Initialized firebase and hide the credentials of firebase in .env

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div>
         <h1 className="text-3xl font-bold text-pink-950 flex  items-center h-screen justify-center">
-          Hello world!
+          Hello world from deployed Firebase App !
         </h1>
       </div>
     </>
