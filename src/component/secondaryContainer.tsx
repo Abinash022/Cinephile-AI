781c207e33e484d4dbb0dbeeb3f893f3e019bea9
Abinash022 <abinashkarki16@gmail.com>
2025-08-19 00:39:10 +0545

    implement Top Rated movies section with custom hook
    - Added custom hook for fetching top rated movies
    - Integrated section UI similar to Trending Now & Newly Released
    - Ensured reusable MovieCard component handles multiple categories

--- src/component/secondaryContainer.tsx
+++ src/component/secondaryContainer.tsx
@@ -1,18 +1,26 @@
 import MovieCard from "./movieCard";
 import useFetchMovies from "../hooks/useFetchMovies";
 import useFetchTrendingMovie from "../hooks/useFetchTrendingMovie";
+import useFetchTopRatedMovies from "../hooks/useFetchTopRatedMovies";
 
 const SecondaryContainer = () => {
   const { movieData } = useFetchMovies();
   const { trendingMovies } = useFetchTrendingMovie();
+  const { topRatedMovies } = useFetchTopRatedMovies();
   return (
     <div className="bg-gradient-to-t from-black via-black to-transparent relative -mt-32 z-10  pb-8">
       <h1 className="text-white font-bold mb-9 text-2xl mx-12">
         Newly Released
       </h1>
       <MovieCard movieData={movieData} />
-      <h1 className="text-white font-bold mb-9 text-2xl mx-12">Trending Now</h1>
+      <h1 className="text-white font-bold mb-9 text-2xl mx-12  mt-9">
+        Trending Now
+      </h1>
       <MovieCard movieData={trendingMovies} />
+      <h1 className="text-white font-bold mb-9 text-2xl mx-12 mt-9">
+        Top Rated Movies
+      </h1>
+      <MovieCard movieData={topRatedMovies} />
     </div>
   );
 };
---
+++ src/hooks/useFetchTopRatedMovies.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { OPTIONS } from "../utils/constant/constant";
+
+const useFetchTopRatedMovies = () => {
+  const [topRatedMovies, setTopRatedMovies] = useState(null);
+  useEffect(() => {
+    async function fetchTopRatedMovies() {
+      const url =
+        "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+      try {
+        const response = await fetch(url, OPTIONS);
+        const json = await response.json();
+        const data = json.results;
+        setTopRatedMovies(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchTopRatedMovies();
+  }, []);
+  return { topRatedMovies };
+};
+
+export default useFetchTopRatedMovies;
